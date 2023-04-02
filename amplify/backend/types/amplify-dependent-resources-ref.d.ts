67dd8cf5a0fd8d756c581727a52de4eca4191581
Hussain <hussimr@emea.corpdir.net>
2023-04-02 19:17:35 +0200

    removed aws resources-auth buckets etc

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,17 +6,6 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIKeyOutput": "string"
     }
   },
-  "auth": {
-    "amplifyapp554c2469": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "storage": {
     "ihnotes": {
       "BucketName": "string",
